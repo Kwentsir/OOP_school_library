98f008bfa26d9ce466e2a07ad1e55ecf7938ad47
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-08-04 21:24:19 +0000

    Add Rental Class

--- person.rb
+++ person.rb
@@ -1,14 +1,14 @@
 require './nameable'
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    super()
+    @rentals = []
   end
 
   private
