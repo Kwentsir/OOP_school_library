8719a73b99756e34b32ee1f9a96931b41b3ae9a0
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-08-05 08:34:26 +0000

    Create student instance

--- app.rb
+++ app.rb
@@ -9,4 +9,11 @@ class App
     @books = []
     @people = []
   end
+
+  def create_student(age, name, parent_permission)
+    student = Student.new(age, name, parent_permission: parent_permission)
+    @people.push(student)
+  end
+
+  
 end
